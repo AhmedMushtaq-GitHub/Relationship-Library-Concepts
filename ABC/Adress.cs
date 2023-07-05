24c59f584a58aaa23bc0df5c2a2f930d9513737f
AhmedMushtaq-GitHub <ahmed@AHMEDMUSHTAQ>
2023-07-05 09:24:58 +0500

    Add project files.

---
+++ ABC/Adress.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ABC
+{
+    public class Adress
+    {
+        public int id;
+        public string name;
+    }
+   public class City
+    { public int id { get; set; }
+        
+      public string name { get; set; }
+    }
+    public class Provence
+    {
+        public int id { get; set; }
+
+        public string name { get; set; }
+    }
+    public class Country
+    {
+        public int id { get; set; }
+
+        public string name { get; set; }
+    }
+}
---
+++ Relationship/Program.cs
@@ -0,0 +1,22 @@
+﻿using ABC;
+
+Adress adress =  new Adress();
+adress.id = 1;
+adress.name = "House 25,Street 7";
+City city = new City();
+city.id = 2;
+city.name = "Faisalabad";
+Provence provence = new Provence();
+provence.id = 3;
+provence.name = "Punjab";
+Country country = new Country();
+country.id = 4;
+country.name = "Pakistan";
+Console.Write($"{adress.id}:My Adress is ");
+Console.WriteLine(adress.name);
+Console.Write($"{city.id}:My City name is ");
+Console.WriteLine(city.name);
+Console.Write($"{provence.id}:My provence name is ");
+Console.WriteLine(provence.name);
+Console.Write($"{country.id}:My Counrty name is ");
+Console.WriteLine(country.name);
